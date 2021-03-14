f67960fcff37d2ae2956bb87e9b80a5335d822e2
Ioannis Christodoulakis <ioannis.ch@outlook.com>
2021-03-14 20:44:44 +0100

    Create Hat.cs

---
+++ tutorial-03/Hat.cs
@@ -0,0 +1,11 @@
+namespace InteractiveScript
+{
+    // Klassen-Namen werden grundsätzlich in Einzahl geschrieben.
+    // Klassen sind üblicherweise in einer gleichnamigen Datei drin.
+    // Verschieben mit: STRG + . oder ALT + ENTER
+    class Hat
+    {
+        public string color;
+    }
+
+}
